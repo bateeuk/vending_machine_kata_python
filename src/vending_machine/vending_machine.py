14dfda88d496952aa2173ecf97484652fae992e1
Jim Kirkbride <james.kirkbride@gmail.com>
2014-05-10 11:02:33 -0400

    Adding nickel test.

--- src/vending_machine/vending_machine.py
+++ src/vending_machine/vending_machine.py
@@ -1,4 +1,18 @@
+NICKEL = "nickel"
+
 class VendingMachine:
 
+  def __init__(self):
+    self.valid_coins = {NICKEL : .05}
+    self.coins = []
+  
+  def accept(self, coin):
+    if (self.valid_coins[coin]):
+      self.coins.append(self.valid_coins[coin])
+
   def display(self):
+    total = sum(self.coins)
+    if (total > 0.0):
+      return "%s" % "{:.2f}".format(total)
+
     return "INSERT COIN"
--- test/accept_coin_tests.py
+++ test/accept_coin_tests.py
@@ -3,6 +3,12 @@ from vending_machine import vending_machine
 
 class AcceptCoinTests(unittest.TestCase):
     
-    def test_given_no_coins_entered_when_total_is_displayed_then_insert_coin_is_displayed(self):
+    def test_when_no_coins_entered_then_insert_coin_is_displayed(self):
         machine = vending_machine.VendingMachine()
         self.assertEquals("INSERT COIN", machine.display())
+    
+    def test_when_nickel_entered_then_five_cents_is_displayed(self):
+        machine = vending_machine.VendingMachine()
+        machine.accept(vending_machine.NICKEL)
+        self.assertEquals("0.05", machine.display())
+
