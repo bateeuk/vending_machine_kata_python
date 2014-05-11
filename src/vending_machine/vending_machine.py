3fd97702be927e00303ed7572caff8d06170c8eb
Jim Kirkbride <james.kirkbride@gmail.com>
2014-05-11 12:14:31 -0400

    Adding first test for product select with candy.  Refactoring display method.

--- src/vending_machine/vending_machine.py
+++ src/vending_machine/vending_machine.py
@@ -1,6 +1,11 @@
+
+''' Valid coins '''
 NICKEL = "nickel"
 DIME = "dime"
 QUARTER = "quarter"
+
+''' Valid products '''
+CANDY = "candy"
 
 class VendingMachine:
 
@@ -8,6 +13,7 @@ class VendingMachine:
     self.valid_coins = {NICKEL : .05, DIME : 0.1, QUARTER : 0.25}
     self.coins = []
     self.invalid_coins = []
+    self.product_selected = False
   
   def accept(self, coin):
     if (self.valid_coins.has_key(coin)):
@@ -16,14 +22,27 @@ class VendingMachine:
       self.invalid_coins.append(coin)
 
   def display(self):
-    total = sum(self.coins)
+    total_coins = sum(self.coins)
+    
+    if self.product_selected:
+        return self.display_with_product_selected(total_coins)
+    else:
+        return self.display_without_product_selected(total_coins)
+    
+  def display_without_product_selected(self, total):
     if (total > 0.0):
       return "%s" % "{:.2f}".format(total)
 
     return "INSERT COIN"
+
+  def display_with_product_selected(self, total):
+      return "PRICE 0.65"
 
   def check_return_slot(self):
     return self.invalid_coins
 
   def clear_return_slot(self):
     self.invalid_coins = []
+
+  def select_product(self, product):
+      self.product_selected = True
---
+++ test/select_product_tests.py
@@ -0,0 +1,12 @@
+import unittest
+from vending_machine import vending_machine
+
+class SelectProductTests(unittest.TestCase):
+
+  def setUp(self):
+    self.machine = vending_machine.VendingMachine()
+  
+  def test_given_no_money_accepted_when_candy_selected_then_PRICE_and_amount_is_displayed(self):
+    self.machine.select_product(vending_machine.CANDY)
+
+    self.assertEqual("PRICE 0.65", self.machine.display())
